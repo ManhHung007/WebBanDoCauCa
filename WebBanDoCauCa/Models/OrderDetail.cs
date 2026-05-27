e3d33c6e21dce82bb5c773b13e4a55ac0548643f
ManhHung007 <manhhung0969420@gmail.com>
2026-05-27 17:56:36 +0700

    last update

--- WebBanDoCauCa/Models/OrderDetail.cs
+++ WebBanDoCauCa/Models/OrderDetail.cs
@@ -6,13 +6,14 @@
 
         // FK Order
         public int OrderId { get; set; }
-        public Order Order { get; set; }
+        public Order? Order { get; set; }
 
         // FK Product
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
 
         public int Quantity { get; set; }
+
         public decimal Price { get; set; }
     }
 }
